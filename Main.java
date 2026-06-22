f424958c4cae65aa6997ce61502986136c9be2cb
Asmitha S.A <kit28.24bcb006@gmail.com>
2026-06-22 21:39:55 +0530

    activities done in day 5

--- Main.java
+++ Main.java
@@ -1,134 +1,124 @@
-package Day04;
+package day05;
 
-public class Main {
+import java.util.*;
 
-    // Linked List Node
-    public static class ListNode {
-        int val;
-        ListNode next;
+class PrefixSuffixProblems {
 
-        ListNode(int val) {
-            this.val = val;
+    // 1. Prefix Sum
+    public static int[] prefixSum(int[] arr) {
+        int n = arr.length;
+        int[] prefix = new int[n];
+
+        prefix[0] = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i - 1] + arr[i];
         }
+
+        return prefix;
     }
 
-    // 1. Merge Two Sorted Linked Lists
-    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode dummy = new ListNode(0);
-        ListNode tail = dummy;
+    // 2. Range Sum Query using Prefix Sum
+    public static int rangeSum(int[] prefix, int left, int right) {
+        if (left == 0) {
+            return prefix[right];
+        }
+        return prefix[right] - prefix[left - 1];
+    }
 
-        while (list1 != null && list2 != null) {
-            if (list1.val <= list2.val) {
-                tail.next = list1;
-                list1 = list1.next;
-            } else {
-                tail.next = list2;
-                list2 = list2.next;
+    // 3. Suffix Sum
+    public static int[] suffixSum(int[] arr) {
+        int n = arr.length;
+        int[] suffix = new int[n];
+
+        suffix[n - 1] = arr[n - 1];
+
+        for (int i = n - 2; i >= 0; i--) {
+            suffix[i] = suffix[i + 1] + arr[i];
+        }
+
+        return suffix;
+    }
+
+    // 4. Equilibrium Point Index
+    public static int equilibriumIndex(int[] arr) {
+        int totalSum = 0;
+
+        for (int num : arr) {
+            totalSum += num;
+        }
+
+        int leftSum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            totalSum -= arr[i]; // right sum
+
+            if (leftSum == totalSum) {
+                return i;
             }
-            tail = tail.next;
+
+            leftSum += arr[i];
         }
 
-        tail.next = (list1 != null) ? list1 : list2;
-
-        return dummy.next;
+        return -1;
     }
 
-    // 2. Merge Sorted Array
-    public static void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = m - 1;
-        int j = n - 1;
-        int k = m + n - 1;
-
-        while (i >= 0 && j >= 0) {
-            if (nums1[i] > nums2[j]) {
-                nums1[k--] = nums1[i--];
-            } else {
-                nums1[k--] = nums2[j--];
-            }
-        }
-
-        while (j >= 0) {
-            nums1[k--] = nums2[j--];
-        }
-    }
-
-    // 3. Maximum Consecutive Ones (Optimized)
-    public static int findMaxConsecutiveOnes(int[] nums) {
-        int count = 0;
-        int maxCount = 0;
+    // 5. Pivot Index (LeetCode 724)
+    public static int pivotIndex(int[] nums) {
+        int totalSum = 0;
 
         for (int num : nums) {
-            if (num == 1) {
-                count++;
-                maxCount = Math.max(maxCount, count);
-            } else {
-                count = 0;
+            totalSum += num;
+        }
+
+        int leftSum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            int rightSum = totalSum - leftSum - nums[i];
+
+            if (leftSum == rightSum) {
+                return i;
             }
+
+            leftSum += nums[i];
         }
 
-        return maxCount;
-    }
-
-    // 4. Maximum Subarray Sum (Kadane's Algorithm)
-    public static int maxSubArray(int[] nums) {
-        int currentSum = nums[0];
-        int maxSum = nums[0];
-
-        for (int i = 1; i < nums.length; i++) {
-            currentSum = Math.max(nums[i], currentSum + nums[i]);
-            maxSum = Math.max(maxSum, currentSum);
-        }
-
-        return maxSum;
-    }
-
-    // Print Linked List
-    public static void printList(ListNode head) {
-        while (head != null) {
-            System.out.print(head.val + " ");
-            head = head.next;
-        }
-        System.out.println();
+        return -1;
     }
 
     public static void main(String[] args) {
 
-        // ===== Question 1 =====
-        System.out.println("Merge Two Sorted Linked Lists:");
+        int[] arr = {1, 2, 3, 4, 5};
 
-        ListNode l1 = new ListNode(1);
-        l1.next = new ListNode(2);
-        l1.next.next = new ListNode(4);
+        // Prefix Sum
+        int[] prefix = prefixSum(arr);
+        System.out.println("Prefix Sum:");
+        System.out.println(Arrays.toString(prefix));
 
-        ListNode l2 = new ListNode(1);
-        l2.next = new ListNode(3);
-        l2.next.next = new ListNode(4);
+        // Range Sum Query
+        int left = 1;
+        int right = 3;
 
-        ListNode mergedList = mergeTwoLists(l1, l2);
-        printList(mergedList);
+        System.out.println("\nRange Sum (" + left + "," + right + "): "
+                + rangeSum(prefix, left, right));
 
-        // ===== Question 2 =====
-        System.out.println("\nMerge Sorted Array:");
+        // Suffix Sum
+        int[] suffix = suffixSum(arr);
+        System.out.println("\nSuffix Sum:");
+        System.out.println(Arrays.toString(suffix));
 
-        int[] nums1 = {1, 2, 3, 0, 0, 0};
-        int[] nums2 = {2, 5, 6};
+        // Equilibrium Index Example
+        int[] eqArr = {-7, 1, 5, 2, -4, 3, 0};
 
-        merge(nums1, 3, nums2, 3);
+        System.out.println("\nEquilibrium Index: "
+                + equilibriumIndex(eqArr));
 
-        for (int num : nums1) {
-            System.out.print(num + " ");
-        }
+        // Pivot Index Example
+        int[] pivotArr = {1, 7, 3, 6, 5, 6};
 
-        // ===== Question 3 =====
-        System.out.println("\n\nMaximum Consecutive Ones:");
-
-        int[] arr1 = {1, 1, 0, 1, 1, 1};
-        System.out.println(findMaxConsecutiveOnes(arr1));
-
-        // ===== Question 4 =====
-        System.out.println("\nMaximum Subarray Sum (Kadane's Algorithm):");
-
-        int[] arr2 = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-        System.out.println(maxSubArray(arr2));
+        System.out.println("Pivot Index: "
+                + pivotIndex(pivotArr));
     }
 }
